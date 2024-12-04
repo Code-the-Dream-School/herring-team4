fa950678c57c6fdab9850d92b70ad7dc066898cf
christopher-ga <chrisga@sas.upenn.edu>
2024-12-04 13:30:33 -0600

    Create friendships table and friendship associations in friendship and user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_222152) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_27_191712) do
   create_table "entries", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "text"
@@ -22,6 +22,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_222152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +47,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_222152) do
   end
 
   add_foreign_key "entries", "users"
+  add_foreign_key "friendships", "users"
+  add_foreign_key "friendships", "users", column: "friend_id"
 end

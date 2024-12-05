713f93bbe5821f9743afdf5dade69d389705a5c7
Mariela-t <tinoco.mariela01@gmail.com>
2024-12-04 22:07:46 -0800

    Worked on user login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show edit update ]
 
   def index
     @users = User.all
@@ -8,18 +8,6 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
 
   def edit
   end
@@ -32,15 +20,11 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully deleted.'
-  end
 
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
   end
 
   def user_params

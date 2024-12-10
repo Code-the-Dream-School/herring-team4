ed86b52bc8a055a3d76ded802affb0e3d969b3ba
JacinthaDev <jacinthadev8@gmail.com>
2024-12-09 19:47:28 -0500

    fix rspec tests for User

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Entry, type: :model do
-  it 'is valid with valid attributes' do
-    user = create(:user)
-    entry = build(:entry, user: user)  
-    expect(entry).to be_valid
+  subject { Entry.new(user: create(:user)) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
   end
 
-  it 'is invalid without a user_id' do
-    entry = build(:entry, user: nil)
-    expect(entry).not_to be_valid
+  it "is not valid without a user_id" do
+    subject.user = nil
+    expect(subject).to_not be_valid
   end
 
-  it 'belongs to a user' do
-    should belong_to(:user)
+  it "belongs to a user" do
+    expect(Entry.reflect_on_association(:user).macro).to eq(:belongs_to)
   end
 
-  it 'has many reactions' do
-    should have_many(:reactions)
+  it "has many reactions" do
+    expect(Entry.reflect_on_association(:reactions).macro).to eq(:has_many)
   end
 end

818e31a0e82eb970cdca1b6f9348600f0aa90b96
christopher-ga <chrisga@sas.upenn.edu>
2024-12-11 21:44:56 -0600

    Add streak check method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,35 @@ class User < ApplicationRecord
     other_user.friendships.find_by(friend: self).destroy
   end
 
+  def check_streak()
 
+    current_date = Date.current
+    current_entry = 0
+    streak = 0
 
+    entry_dates = self.entries.select("DATE(created_at) AS unique_date").group("DATE(created_at)").order(created_at: :desc)
+
+    # check if date of first entry is today, if so, add 1 to streak
+    if Date.parse(entry_dates[current_entry].unique_date) == current_date
+      current_entry = current_entry + 1
+      streak = streak + 1
+    end
+
+    current_date = current_date - 1
+
+    is_streak = true
+    while is_streak
+      if Date.parse(entry_dates[current_entry].unique_date) == current_date
+        current_entry = current_entry + 1
+        streak = streak + 1
+        current_date = current_date - 1
+      else
+        is_streak = false
+      end
+
+    end
+
+    puts streak
+
+  end
 end

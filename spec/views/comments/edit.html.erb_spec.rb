f9d24eeda70efc8923318d301379432df0cbc699
Mariela-t <tinoco.mariela01@gmail.com>
2024-12-23 15:57:49 -0800

    added testing for views folder: edit and index files

--- spec/views/comments/edit.html.erb_spec.rb
+++ spec/views/comments/edit.html.erb_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "comment/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "comments/edit.html.erb", type: :view do
+let(:user) { FactoryBot.create(:user) }
+let(:entry) { FactoryBot.create(:entry, user: user) }
+let(:comment) { FactoryBot.create(:comment, entry: entry, user: user, text: "Test comment") }
+
+before do 
+    assign(:entry, entry)
+    assign(:comment, comment)
+    render 
+  end
+
+it "displays the heading" do
+  expect(rendered).to have_selector("h1", text: "Edit comment")
+end
+
+it "renders the form to edit comment" do
+expect(rendered).to have_selector("form[action='#{entry_comment_path(entry, comment)}'][method='post']")
+end
+
+it "pre-fills the text area with the current comment" do
+  expect(rendered).to have_selector("textarea[name='comment[text]']", text: comment.text)
+end
+
+it "displays the submit button" do
+  expect(rendered).to have_selector("input[type='submit'][value='Update']")
+end
+
+it "has a link to show the comment" do
+  expect(rendered).to have_link('Show this comment', href: entry_comments_path(entry, comment))
+end 
+
+it "has a link to go back to the comments index" do
+expect(rendered).to have_link('Back to comment', href: entry_comments_path(entry))
+end
 end
--- spec/views/comments/index.html.erb_spec.rb
+++ spec/views/comments/index.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "comment/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "comments/index.html.erb", type: :view do
+let(:user) { FactoryBot.create(:user) }
+let(:entry) { FactoryBot.create(:entry, user: user) }
+let!(:comments) { FactoryBot.create_list(:comment, 2, entry: entry, user: user) }
+
+before do 
+    assign(:entry, entry)
+    assign(:comments, comments)
+    render 
+  end
+
+  it "displays the entry" do
+  expect(rendered).to include(entry.text)
+end
+
+  it "displays all comments for that entry" do
+    comments.each do |comment|
+      expect(rendered).to include(comment.user.email)
+      expect(rendered).to include(comment.text)
+    end
+  end
+
+  it "displays a link back to the entry" do
+    expect(rendered).to have_link("Back to entry", href: entry_path(entry))
+  end
+
 end

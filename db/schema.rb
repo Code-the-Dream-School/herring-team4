aa594e2eafabbe0cf0b27d8f0b2b5de345084a01
JacinthaDev <jacinthadev8@gmail.com>
2024-12-28 15:00:49 -0500

    changed first name & last name to username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_27_191712) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_28_190322) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "entry_id", null: false
@@ -49,7 +49,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_27_191712) do
     t.text "emote"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["entry_id"], name: "index_reactions_on_entry_id"
+    t.index ["user_id"], name: "index_reactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,8 +62,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_27_191712) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "entries"
@@ -70,4 +75,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_27_191712) do
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "reactions", "entries"
+  add_foreign_key "reactions", "users"
 end

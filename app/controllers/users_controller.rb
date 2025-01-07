e22883f11307fa8821f8377020da9223c3e8d186
JacinthaDev <jacinthadev8@gmail.com>
2025-01-06 23:29:03 -0500

    complete user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update ]
+  before_action :set_user, only: %i[show edit update update_bio]
+  before_action :set_user_for_upload, only: [:upload_profile_picture]
 
   def index
     @users = User.all
   end
 
   def show
+    @friends = @user.friends
+    @entries = @user.entries
   end
-
 
   def edit
   end
@@ -20,11 +22,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def upload_profile_picture
+    if params[:user][:profile_picture].present?
+      @user.profile_picture.attach(params[:user][:profile_picture])
+      redirect_to @user, notice: 'Profile picture uploaded successfully.'
+    else
+      redirect_to @user, alert: 'No picture selected.'
+    end
+  end
+
+  def update_bio
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Bio updated successfully.'
+    else
+      render :show
+    end
+  end
 
   private
 
   def set_user
-    @user = User.find_by(id: params[:id])
+    @user = current_user
+  end
+
+  def set_user_for_upload
+    @user = User.find(params[:id])
   end
 
   def user_params
